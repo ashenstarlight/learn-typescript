f42a5645389ce8efa3df424dbdf7353240d2698e
Emily <vishalrshinde@acpce.ac.in>
2024-03-29 16:57:36 +0530

    Why TS is required

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
-var a: number = 66
-console.log(a);
+import axios from "axios"
+const url = 'https://jsonplaceholder.typicode.com/todos/2'
 
+// Response data has properties of:
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then(response => {
+ const todo = response.data as Todo;
+
+ const ID = todo.id;
+ const title = todo.title;
+ const finished = todo.completed
+
+ console.log(`
+    Todo with ID: ${ID}
+    has a title of: ${title}
+    is it finished? ${finished}
+ `);
+ 
+})    
